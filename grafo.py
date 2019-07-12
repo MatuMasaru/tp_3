ed6c8c562d8493d03050ce9db0de0f0359272595
yimy llanos puita <yllanos@fi.uba.ar>
2019-07-12 19:05:45 -0300

    grafo modificado

--- grafo.py
+++ grafo.py
@@ -7,7 +7,6 @@ class Grafo:
         self.vertices = {}
 
     def vertice_pertenece(self, vertice):
-        """devuelve true si el vertice pertenece , o false en caso contrario"""
         return vertice in self.vertices
 
     def agregar_vertice(self, nuevo_vertice):
@@ -33,25 +32,21 @@ class Grafo:
         return False
 
     def esta_conectado(self, vertice_1, vertice_2):
-        if self.vertice_pertenece(vertice_1) and self.vertice_pertenece(vertice_2):#si ambos pertenecen al grafo
-            
-           # return vertice_1 in self.vertices[vertice_2]    #el mensaje es unidireccional, de vertice_1 a vertice_2
-            #        **miralo yimi**                             #solo ipporta en esta direccion si esta conectado
+        if self.vertice_pertenece(vertice_1) and self.vertice_pertenece(vertice_2):
             if vertice_2 in self.adyacentes(vertice_1):
                 return True
         return False
 
 
     def agregar_arista(self, salida_vertice, entrada_vertice, peso=1):
-        if not self.esta_conectado(salida_vertice, entrada_vertice):    #SI NO ESTA CONECTADO, agrego(seria los corchetes) 
-            self.vertices[entrada_vertice][salida_vertice] = peso       #y le coloco el peso 
+        if not self.esta_conectado(salida_vertice, entrada_vertice):
+            self.vertices[entrada_vertice][salida_vertice] = peso
             return True
         return False
 
     def remover_arista(self, vertice_1, vertice_2): #deberia devolverse la arista
         if not self.esta_conectado(vertice_1, vertice_2): #si no esta no se remueve
-            return self.vertices[vertice_1].pop(vertice_2)   #sacar arista y devolverlo 
-        return None
+            self.vertices[vertice_1].pop(vertice_2)   #sacar arista y devolverlo 
 
     def obtener_vertices(self):
         return self.vertices.keys()
